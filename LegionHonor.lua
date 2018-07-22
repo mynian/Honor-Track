da457ff275cb6b7eb90c366acd3a435f0f2f615f
mynian <mynian@gmail.com>
2018-07-22 14:43:53 -0400

    bfa changes

--- LegionHonor.lua
+++ LegionHonor.lua
@@ -1,7 +1,7 @@
 --Create the Frame
 local Addon, events = CreateFrame("Frame", "LegionHonor", UIParent), {};
 Addon:SetWidth(175);
-Addon:SetHeight(180);
+Addon:SetHeight(100);
 Addon:SetPoint("CENTER", UIParent, "CENTER");
 Addon:SetMovable(true);
 Addon:EnableMouse(true);
@@ -22,31 +22,26 @@ Addon:SetBackdropColor(0,0,0,.8)
 Addon:SetBackdropBorderColor(1,1,1,1)
 
 --Add the text
-Addon.PrestigeLevelText = Addon:CreateFontString("LegionHonor_PrestigeText", "OVERLAY", "GameFontNormal");
-Addon.PrestigeLevelText:SetPoint("LEFT", 2, 60);
-Addon.PrestigeLevelText:SetText("Prestige Level");
 Addon.HonorLevelText = Addon:CreateFontString("LegionHonor_HonorLevelText", "OVERLAY", "GameFontNormal");
-Addon.HonorLevelText:SetPoint("LEFT", 2, 30);
+Addon.HonorLevelText:SetPoint("LEFT", 2, 28);
 Addon.HonorLevelText:SetText("Honor Level");
 Addon.HonorAmountText = Addon:CreateFontString("LegionHonor_HonorText", "OVERLAY", "GameFontNormal");
-Addon.HonorAmountText:SetPoint("LEFT", 2, 0);
+Addon.HonorAmountText:SetPoint("LEFT", 2, 8);
 Addon.HonorAmountText:SetText("Current Honor");
 Addon.HonorGoalText = Addon:CreateFontString("LegionHonor_HonorGoalText", "OVERLAY", "GameFontNormal");
-Addon.HonorGoalText:SetPoint("LEFT", 2, -30);
+Addon.HonorGoalText:SetPoint("LEFT", 2, -12);
 Addon.HonorGoalText:SetText("Honor to Farm");
 Addon.HonorPerHourText = Addon:CreateFontString("LegionHonor_HonorPerHourText", "OVERLAY", "GameFontNormal");
-Addon.HonorPerHourText:SetPoint("LEFT", 2, -60);
+Addon.HonorPerHourText:SetPoint("LEFT", 2, -32);
 Addon.HonorPerHourText:SetText("Honor per Hour");
-Addon.PlayerPrestigeLevel = Addon:CreateFontString("LegionHonor_PlayerPrestigeLevel", "OVERLAY", "GameFontNormal");
-Addon.PlayerPrestigeLevel:SetPoint("RIGHT", -2, 60);
 Addon.PlayerHonorLevel = Addon:CreateFontString("LegionHonor_PlayerHonorLevel", "OVERLAY", "GameFontNormal");
-Addon.PlayerHonorLevel:SetPoint("RIGHT", -2, 30);
+Addon.PlayerHonorLevel:SetPoint("RIGHT", -2, 28);
 Addon.PlayerHonorAmount = Addon:CreateFontString("LegionHonor_PlayerHonor", "OVERLAY", "GameFontNormal");
-Addon.PlayerHonorAmount:SetPoint("RIGHT", -2, 0);
+Addon.PlayerHonorAmount:SetPoint("RIGHT", -2, 8);
 Addon.HonorGoalAmount = Addon:CreateFontString("LegionHonor_HonorGoalAmount", "OVERLAY", "GameFontNormal");
-Addon.HonorGoalAmount:SetPoint("RIGHT", -2, -30);
+Addon.HonorGoalAmount:SetPoint("RIGHT", -2, -12);
 Addon.HonorPerHourAmount = Addon:CreateFontString("LegionHonor_HonorPerHourAmount", "OVERLAY", "GameFontNormal");
-Addon.HonorPerHourAmount:SetPoint("RIGHT", -2, -60);
+Addon.HonorPerHourAmount:SetPoint("RIGHT", -2, -32);
 
 
 --Create Function to round the decimals
@@ -113,9 +108,7 @@ function SlashCmdList.LEGIONHONOR(msg, editBox)
 	elseif string.lower(command) == 'goal' and string.lower(rest) == "reset" then
 		lhhonorgoal = 0
 		UpdateGoal(self)
-		print("Legion Honor: Honor Goal reset")
-	--[[elseif string.lower(command) == 'debug' and string.match(rest, "%d*") ~= nil and string.match(rest, "%a") == nil then
-		lhhonorgained = string.match(rest, "%d*")]]
+		print("Legion Honor: Honor Goal reset")	
 	else 
 		print("Legion Honor: Available commands are show, hide and goal")
 		print("Legion Honor: To set goal, use /legionhonor goal ####")
@@ -130,18 +123,13 @@ local lhhonor, lhhonormax, lhhonorlevel;
 --Function to pull honor amounts
 local function UpdateHonor(self)
 	--Pull Honor Amounts
-	local lhprestige, lhhonorlevelmax, lhprestigemax;
-	lhprestige = UnitPrestige("Player");
+	local lhhonorlevelmax, lhprestigemax;	
 	lhhonor = UnitHonor("player");
 	lhhonormax = UnitHonorMax("player")
-	lhhonorlevel = UnitHonorLevel("player")
-	lhhonorlevelmax = GetMaxPlayerHonorLevel();
-	lhprestigemax = GetMaxPrestigeLevel();
-	
-	-- Set the outputs
-	self.PlayerPrestigeLevel:SetText(lhprestige .. "/" .. lhprestigemax);	
+	lhhonorlevel = UnitHonorLevel("player")	
+	-- Set the outputs		
 	self.PlayerHonorAmount:SetText(lhhonor .. "/" .. lhhonormax);	
-	self.PlayerHonorLevel:SetText(lhhonorlevel .. "/" .. lhhonorlevelmax );
+	self.PlayerHonorLevel:SetText(lhhonorlevel);
 	
 		
 end
@@ -195,8 +183,7 @@ local function OnUpdate(self, elapsed)
 		lhcounter = 0
 		lhhonorperhour = lhhonorgained / lhtimer * 3600
 		lhhonorperhour = mathround(lhhonorperhour, 2)
-		Addon.HonorPerHourAmount:SetText(lhhonorperhour)
-		--print(lhhonorperhour)
+		Addon.HonorPerHourAmount:SetText(lhhonorperhour)		
 	end	
 end
 
